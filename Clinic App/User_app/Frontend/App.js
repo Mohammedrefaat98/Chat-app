e1f4ed6a5f6c3b3d3f850beeaa3ada780c013dbf
refaat <mohammed.refaat@epicore.co>
2021-10-14 01:10:57 +0200

    Updating ChatList

--- Clinic App/User_app/Frontend/App.js
+++ Clinic App/User_app/Frontend/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ChatList from './src/screens/ChatList';
+import Chatlist from './src/screens/Chatlist';
 export default class App extends React.Component {
   render() {
-    return <ChatList />;
+    return <Chatlist />;
   }
 }
--- Clinic App/User_app/Frontend/src/screens/ChatList.js
+++ Clinic App/User_app/Frontend/src/screens/ChatList.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, TextInput} from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
 import { createIconSetFromFontello } from 'react-native-vector-icons';
 import fontelloConfig from '../config.json';
 const Icon = createIconSetFromFontello(fontelloConfig);
+import { ChatItem } from 'react-chat-elements/native'
 
-function ChatList(props) {
-
+function Chatlist(props) {
     return (
         <View style={styles.container}>
             <View style={styles.SearchContainer}>
                 <View style={styles.SearchBar}>
-                    <Icon name="search" size={15.78} style={styles.Searchicon} />
-                    <Icon name="filter" size={30} style={styles.Filtericon} />
+                    <Icon name="search" size={16} style={styles.Searchicon} />
+                    <Icon name="filter" size={17} style={styles.Filtericon} />
                     <TextInput
                         style={styles.SearchInput}
                         placeholder="Search"
                     />
                 </View>
+                <Text style={styles.Cancel}>Cancel</Text>
+            </View>
+            <View style={styles.Chatlist}>
+                <ChatItem
+                    avatar={'https://facebook.github.io/react/img/logo.svg'}
+                    alt={'Reactjs'}
+                    title={'Facebook'}
+                    subtitle={'What are you doing?'}
+                    date={new Date()}
+                    unread={0} />
             </View>
         </View>
     );
@@ -26,20 +36,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    Chatlist: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height - 116
+    },
+    Cancel: {
+        fontSize: 17,
+        fontWeight: '400',
+        top: 15,
+        left: 0.805 * Dimensions.get('window').width,
+        color: '#007AFF'
+    },
     SearchBar: {
         top: 10,
         left: 16,
-        width: 0.728*Dimensions.get('window').width,
+        width: 0.728 * Dimensions.get('window').width,
         height: 36,
         borderRadius: 10,
         position: 'absolute',
         backgroundColor: '#EBEBF5',
     },
-    SearchInput:{
-        left:30,
-        top:2,
-        height:36,
-        width: 0.53*Dimensions.get('window').width
+    SearchInput: {
+        fontSize: 17,
+        fontWeight: '400',
+        left: 30,
+        top: -4,
+        height: 45,
+        width: 0.53 * Dimensions.get('window').width,
     },
     SearchContainer: {
         width: Dimensions.get('window').width,
@@ -47,14 +70,19 @@ const styles = StyleSheet.create({
     },
     Searchicon: {
         left: 8.18,
-        top: 10,
+        top: 9.98,
+        width: 15.63,
+        height: 15.78,
         position: 'absolute',
     },
     Filtericon: {
-        left: 0.61*Dimensions.get('window').width,
-        top: 5,
+        left: (0.66 * Dimensions.get('window').width) - 3,
+        top: 10,
+        width: 30,
+        height: 30,
         position: 'absolute',
     },
+
 });
 
-export default ChatList;
+export default Chatlist;
