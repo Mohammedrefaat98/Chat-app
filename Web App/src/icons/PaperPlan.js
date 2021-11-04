a766e9fb0ae9752227679afb07600e34a13203d8
mohammed refaat <mohammed.refaat@epicore.co>
2021-11-04 16:47:17 +0200

    Adding New Icons

---
+++ Web App/src/icons/PaperPlan.js
@@ -0,0 +1,17 @@
+import createSvgIcon from '@material-ui/core/utils/createSvgIcon';
+
+const Paperclip = createSvgIcon(
+  <svg
+    viewBox="0 0 31 28"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 26.4511C0 27.1774 0.749716 27.6614 1.41159 27.3625L28.982 14.9114C29.7665 14.5571 29.7665 13.4429 28.982 13.0886L1.41159 0.637492C0.749719 0.338583 0 0.822625 0 1.54886V10.137C0 10.5823 0.294501 10.974 0.722333 11.0977L7.44011 13.0393C8.40322 13.3177 8.40323 14.6823 7.44012 14.9607L0.722334 16.9023C0.294501 17.026 0 17.4177 0 17.863V26.4511Z"
+    />
+  </svg>, 'Paperclip'
+);
+
+export default Paperclip;
