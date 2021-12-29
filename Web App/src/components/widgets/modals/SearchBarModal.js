c31f17f8d7c1604bc155d512a31d2a107feebf84
mohammed refaat <mohammed.refaat@epicore.co>
2021-12-29 13:42:58 +0200

    change filenames to human readable names

---
+++ Web App/src/components/widgets/modals/SearchBarModal.js
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Button,
+  Container,
+  InputAdornment,
+  Paper,
+  TextField
+} from '@material-ui/core';
+import SearchIcon from '../../../icons/Search';
+import AdjustmentIcon from '../../../icons/Adjustments';
+
+const Modal3 = () => (
+  <Box
+    sx={{
+      backgroundColor: 'background.default',
+      p: 3,
+      textAlign: '-webkit-center'
+    }}
+  >
+    <Paper elevation={12} sx={{ flex: 1, textAlign: '-webkit-center', justifyContent: 'center', width: '80%', maxWidth: 'md', minWidth: '400px' }}>
+      <Box sx={{ p: 3 }}>
+        <Container width="30%" maxWidth="md" sx={{ textAlign: '-webkit-center' }}>
+          <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex'
+            }}
+          >
+            <TextField
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon fontSize="small" />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <AdjustmentIcon fontSize="small" />
+                  </InputAdornment>
+                )
+              }}
+              placeholder="Search..."
+            />
+            <Button
+              color="primary"
+              size="large"
+              sx={{ ml: 2 }}
+              variant="contained"
+            >
+              Search
+            </Button>
+          </Box>
+        </Container>
+      </Box>
+    </Paper>
+  </Box>
+);
+
+export default Modal3;
