eeff848a8db6e89546d88d2e19e313b11e8bef59
mohammed refaat <mohammed.refaat@epicore.co>
2021-12-29 21:58:13 +0200

    Modify source code

--- Web App/src/App.js
+++ Web App/src/App.js
@@ -1,10 +1,11 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { Box, CssBaseline, ThemeProvider } from '@material-ui/core';
 import './i18n';
 import RTL from './components/RTL';
 import SettingsDrawer from './components/SettingsDrawer';
 import useSettings from './hooks/useSettings';
 import { createCustomTheme } from './theme';
-import Modal3 from './components/widgets/modals/SearchBarModal';
+import SearchBar from './components/widgets/modals/SearchBarModal';
+import ContactList from './components/widgets/modals/ContactListModal';
 import ChatModal from './components/widgets/modals/ChatRoomModal';
 
 const App = () => {
@@ -22,8 +23,16 @@ const App = () => {
       <RTL direction={settings.direction}>
         <CssBaseline />
         <SettingsDrawer />
-        <Modal3 />
-        <ChatModal />
+        <SearchBar />
+        <Box
+          sx={{
+            flexDirection: 'row',
+            display: 'flex',
+          }}
+        >
+          <ContactList />
+          <ChatModal />
+        </Box>
       </RTL>
     </ThemeProvider>
   );
