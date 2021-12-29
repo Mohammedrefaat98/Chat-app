22d3fca14b3cd74830bd0f3030c208052b00291e
mohammed refaat <mohammed.refaat@epicore.co>
2021-12-29 13:44:37 +0200

    remove unusual components from source code

--- Web App/src/App.js
+++ Web App/src/App.js
@@ -1,24 +1,14 @@
-import { useEffect } from 'react';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import './i18n';
 import RTL from './components/RTL';
 import SettingsDrawer from './components/SettingsDrawer';
-import { gtmConfig } from './config';
-import useScrollReset from './hooks/useScrollReset';
 import useSettings from './hooks/useSettings';
-import gtm from './lib/gtm';
 import { createCustomTheme } from './theme';
-import Modal3 from './components/widgets/modals/Modal3';
-import ChatModal from './components/widgets/modals/ChatModal';
+import Modal3 from './components/widgets/modals/SearchBarModal';
+import ChatModal from './components/widgets/modals/ChatRoomModal';
 
 const App = () => {
   const { settings } = useSettings();
-
-  useScrollReset();
-
-  useEffect(() => {
-    gtm.initialize(gtmConfig);
-  }, []);
 
   const theme = createCustomTheme({
     direction: settings.direction,
--- Web App/src/index.js
+++ Web App/src/index.js
@@ -5,32 +5,22 @@ import 'nprogress/nprogress.css';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
 import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
 import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
 import App from './App';
-import { AuthProvider } from './contexts/JWTContext';
 import { SettingsProvider } from './contexts/SettingsContext';
-import store from './store';
 
 ReactDOM.render(
   <StrictMode>
     <HelmetProvider>
-      <ReduxProvider store={store}>
-        <StyledEngineProvider injectFirst>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <SettingsProvider>
-              <BrowserRouter>
-                <AuthProvider>
-                  <App />
-                </AuthProvider>
-              </BrowserRouter>
-            </SettingsProvider>
-          </LocalizationProvider>
-        </StyledEngineProvider>
-      </ReduxProvider>
+      <StyledEngineProvider injectFirst>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <SettingsProvider>
+            <App />
+          </SettingsProvider>
+        </LocalizationProvider>
+      </StyledEngineProvider>
     </HelmetProvider>
   </StrictMode>, document.getElementById('root')
 );
